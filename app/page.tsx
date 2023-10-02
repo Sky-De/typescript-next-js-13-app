7fa767d9ce98817e1cae22fc679bfd751fa8d7a6
SkyDe <sky_de1991@outlook.com>
2023-10-03 00:06:13 +0330

    metadata added to home page / title , description, keywords

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Hero from "@components/hero/Hero";
 import Posts from "@components/posts/Posts";
 
+export const metadata = {
+  title: "AI-Share",
+  description:
+    "Explore the boundless potential of AI with a global network of like-minded individuals, all eager to connect, collaborate, andcollectively shape the future of machine intelligence",
+  keywords: "ai, share, skyde1991, app",
+};
+
 const Home = () => {
   return (
     <section className="home__page w-full">
